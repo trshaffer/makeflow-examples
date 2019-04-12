d0b85bb61dd9d2e37e056906402b4489ece7c722
Tim Shaffer <tshaffe1@nd.edu>
2019-04-12 12:33:50 -0400

    Make safe copies rather than move reference files

--- lifemapper/fixref.sh
+++ lifemapper/fixref.sh
@@ -3,7 +3,9 @@
 for a in worldclim mid lgm; do
   mkdir -p "$a"
   for b in $(find . -type f -name "*.$a"); do
-    mv "$b" "$a/${b%.$a}.asc"
+    T=$(mktemp -p "$a")
+    cp "$b" "$T"
+    mv "$T" "$a/${b%.$a}.asc"
   done
 done
 
