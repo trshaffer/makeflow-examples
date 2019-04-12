a62429c7c3f0aa46398c6f8a504db4c11ea84916
Tim Shaffer <tshaffe1@nd.edu>
2019-04-12 11:11:30 -0400

    Make sure reference fixup works when run multiple times concurrently

--- lifemapper/fixref.sh
+++ lifemapper/fixref.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 for a in worldclim mid lgm; do
-  mkdir -p $a
-  cp *.$a $a
-  rename .$a .asc $a/*
+  mkdir -p "$a"
+  for b in $(find . -type f -name "*.$a"); do
+    mv "$b" "$a/${b%.$a}.asc"
+  done
 done
 
 exit 0
